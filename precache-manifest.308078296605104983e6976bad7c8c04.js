a9bd652fe6df137524bb74a417da4e04d9593569
Signior-X <b19188@students.iitmandi.ac.in>
2020-10-14 11:28:51 +0530

    Updates

---
+++ precache-manifest.308078296605104983e6976bad7c8c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43053c6b0426f48b23778fa8be9c72de",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "468a53ba45b78380cd48",
+    "url": "/jeopardy/static/css/main.bc31f057.chunk.css"
+  },
+  {
+    "revision": "d9fcecb41aa1203a490a",
+    "url": "/jeopardy/static/js/2.0d49a7c8.chunk.js"
+  },
+  {
+    "revision": "88c7adc58448a2661ef1f8f7fb418351",
+    "url": "/jeopardy/static/js/2.0d49a7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468a53ba45b78380cd48",
+    "url": "/jeopardy/static/js/main.5a634d05.chunk.js"
+  },
+  {
+    "revision": "20628f3ca52cc50d4f55",
+    "url": "/jeopardy/static/js/runtime-main.0a9d7bb0.js"
+  }
+]);
